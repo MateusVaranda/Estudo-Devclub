9e1a54a65f650ad6a84270284ed3578ba073e63f
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-04-17 20:42:29 -0400

    Template Strings

---
+++ JavaScript/templateStrings.js
@@ -0,0 +1,7 @@
+const idade = 25
+const nome = 'Mateus'
+const sobrenome = 'Varanda'
+const formacao = 'Desenvolvedor Web'
+
+const apresentacap = `Olá, meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e sou ${formacao}.`
+console.log(apresentacap)
