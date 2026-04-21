e0b51f58fb2c60f724272da280960a7772fbf90d
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-04-20 23:07:39 -0400

    if e Else

---
+++ JavaScript/array.js
@@ -0,0 +1,4 @@
+let frutas = ["Banana", "maçã", "laranja", "uva"];
+console.log(frutas[2]); // Acessando o terceiro elemento do array
+console.log(frutas.length); // Obtendo o tamanho do array
+
---
+++ JavaScript/if-else.js
@@ -0,0 +1,22 @@
+let idade = 18
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+console.log("Bem-vindo a nossa festa! Voce e permitido entrar. Voce tem " + idade + " anos.");
+
+let numero = 10
+console.log( numero == 10)
+
+let numero2 = 5
+console.log( numero2 === "5")
+
+let numero3 = 5
+console.log( numero3 === 5)
+
+let numero4 = 5
+console.log( numero4 != 10)
+
+let numero5 = 5
+console.log( numero5 !== "5")
