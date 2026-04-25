ce16965cb047ee8594cd0e3c649ec54b82ffb2a1
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-04-24 20:42:25 -0400

    estilizando com JavaScript

--- JavaScript/alterandoelementos/alter.js
+++ JavaScript/alterandoelementos/alter.js
@@ -1,3 +1,22 @@
-const element = document.querySelector("#main-input");
-element.placeholder = "Digite seu nome completo";
-console.log(element.placeholder);
+
+
+
+// -> Exemplo 1: Alterando o conteúdo de um elemento
+// const element = document.querySelector("#main-input"); // Seleciona o elemento de entrada com o ID "main-input"
+// element.placeholder = "Digite seu nome completo"; // Altera o valor do atributo "placeholder" do elemento de entrada
+// console.log(element.placeholder);   // Saída: "Digite seu nome completo"
+
+
+// -> Exemplo 2: Alterando elemento
+// textContent: Altera o conteúdo de texto de um elemento
+// innerHTML: Altera o conteúdo HTML de um elemento
+// innerText: Altera o conteúdo de texto visível de um elemento, ignorando elementos ocultos e estilos CSS
+
+
+const element = document.querySelector(".paragraph-js"); // Seleciona o elemento com a classe "paragraph-js"
+element.innerHTML = "O conteúdo de texto foi alterado!"; // Altera o conteúdo de texto do elemento
+console.log(element.innerHTML); // Saída: "O conteúdo de texto foi alterado!"
+
+const button = document.querySelector("#btn"); // Seleciona o elemento de botão com o ID "btn"
+button.style.backgroundColor = "blue"; // Altera a cor de fundo do botão para azul
+button.style.color = "white"; // Altera a cor do texto do botão para branco
