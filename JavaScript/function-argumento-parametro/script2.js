9ee99ecf5da32fb88d578e3391674eb4eaba1fa3
Mateus Varanda Ferreira de Carvalho <mateus.varanda.ferreira@gmail.com>
2026-05-17 10:15:35 -0400

    function

---
+++ JavaScript/function-argumento-parametro/script2.js
@@ -0,0 +1,5 @@
+function soma(value1, value2) {
+    console.log(value1 - value2);
+}
+
+soma(5, 10)
---
+++ JavaScript/return/script.js
@@ -0,0 +1,4 @@
+function soma (valor1 = 15, valor2 = 10) {
+    return valor1 + valor2;
+}
+soma()
